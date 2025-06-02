da50ad6dd4c0454d1dec486537cd8386022b9d12
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-01 20:50:56 -0600

    FondoenMenu

--- Proyecto2_Programacion_Samuel_Leonardo/src/proyecto2_programacion_samuel_leonardo/menuPantalla.java
+++ Proyecto2_Programacion_Samuel_Leonardo/src/proyecto2_programacion_samuel_leonardo/menuPantalla.java
@@ -3,18 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package proyecto2_programacion_samuel_leonardo;
-
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 /**
  *
  * @author hnleo
  */
 public class menuPantalla extends javax.swing.JFrame {
-
+    ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("assets/fondoMenu.png"));
+    
     /**
      * Creates new form menuPantalla
      */
     public menuPantalla() {
         initComponents();
+        jLabel1.setIcon(imageIcon);
     }
 
     /**
@@ -26,17 +29,25 @@ public class menuPantalla extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -78,5 +89,6 @@ public class menuPantalla extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
