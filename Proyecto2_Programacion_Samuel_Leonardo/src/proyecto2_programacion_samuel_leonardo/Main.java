4e42b1cb07ee9e48bbc8a69b81d59b8c8b0adc8a
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-06-11 12:03:56 -0600

    tablero

--- Proyecto2_Programacion_Samuel_Leonardo/src/proyecto2_programacion_samuel_leonardo/Main.java
+++
@@ -1,8 +0,0 @@
-package proyecto2_programacion_samuel_leonardo;
-
-public class Main {
-   public static void main(String[] args){
-       menuPantalla menu = new menuPantalla();
-       menu.setVisible(true);
-   }
-}
