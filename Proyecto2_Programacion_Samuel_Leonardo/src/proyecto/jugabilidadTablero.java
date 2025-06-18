c11e1768bf51b64f060bcd70b7895f1ccc71d2eb
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-06-18 06:50:13 -0600

    jubagilidad

--- Proyecto2_Programacion_Samuel_Leonardo/src/proyecto/jugabilidadTablero.java
+++ Proyecto2_Programacion_Samuel_Leonardo/src/proyecto/jugabilidadTablero.java
@@ -14,55 +14,149 @@ import java.awt.event.ActionEvent;
  */
 public class jugabilidadTablero {
 
-    private JButton primerBotonSelecc = null;
+    private casillas primeraCasillaSelecc = null;
     private boolean esperarSegundoClic = false;
 
-    private JButton[][] tablero;
+    private casillas[][] tablero;
 
-    public jugabilidadTablero(JButton[][] tablero) {
+    public jugabilidadTablero(casillas[][] tablero) {
         this.tablero = tablero;
         manejarClics();
     }
 
     private void manejarClics() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                JButton boton = tablero[i][j];
-                boton.addActionListener(new ActionListener() {
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                casillas casilla = tablero[i][j];
+                casilla.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        manejarClicBoton((JButton) e.getSource());
-
+                        manejarClicCasilla((casillas) e.getSource());
                     }
-
                 });
+            }
+        }
+    }
+
+    private void manejarClicCasilla(casillas casillaSelecc) {
+        if (!esperarSegundoClic) {
+            // Primer clic
+            if (casillaSelecc.tienePersonaje()) {
+                primeraCasillaSelecc = casillaSelecc;
+                esperarSegundoClic = true;
+
+            } else {
+
+            }
+        } else {
+            // Segundo clic
+            casillas segundaCasilla = casillaSelecc;
+
+            // Verificar si es un movimiento válido
+            if (esMovimientoValido(primeraCasillaSelecc, segundaCasilla)) {
+                moverPersonaje(primeraCasillaSelecc, segundaCasilla);
+
+            } else {
 
             }
 
-        }
-
-    }
-
-    private void manejarClicBoton(JButton botonSelecc) {
-        if (!esperarSegundoClic) {
-            primerBotonSelecc = botonSelecc;
-            esperarSegundoClic = true;
-            System.out.println("Esperando segundo Boton");
-        } else {
-            JButton segundo = botonSelecc;
-            cambioDeBoton(primerBotonSelecc, segundo);
-            primerBotonSelecc = null;
+            // Resetear selección
+            primeraCasillaSelecc = null;
             esperarSegundoClic = false;
-            System.out.println("Boton Cambiado");
-
         }
     }
 
-    public void cambioDeBoton(JButton boton1, JButton boton2) {
-        Icon temp = boton1.getIcon();
-        boton1.setIcon(boton2.getIcon());
-        boton2.setIcon(temp);
-        
+    private boolean esMovimientoValido(casillas origen, casillas destino) {
+        // No puede mover a la misma casilla
+        if (origen == destino) {
+            System.out.println("Misma casilla seleccionada");
+            return false;
+        }
+
+        // Verificar si el personaje puede moverse
+        if (!origen.getPersonaje().isPuedeMover()) {
+            System.out.println("Personaje no puede moverse");
+            return false;
+        }
+
+        // Si la casilla destino tiene personaje, verificar si es del equipo contrario
+        if (destino.tienePersonaje()) {
+            boolean mismoEquipo = origen.getPersonaje().isEsHeroe() == destino.getPersonaje().isEsHeroe();
+            if (mismoEquipo) {
+                System.out.println("No puedes atacar a tu propio equipo");
+                return false;
+            } else {
+
+                return true;
+            }
+        }
+
+        return true; // Movimiento a casilla vacía
+    }
+
+    private void moverPersonaje(casillas origen, casillas destino) {
+        // Obtener el personaje de la casilla origen
+        Fichas personajeAMover = origen.getPersonaje();
+
+        // Si la casilla destino tiene un personaje (combate)
+        if (destino.tienePersonaje()) {
+            Fichas personajeDefensor = destino.getPersonaje();
+
+            // Resolver combate
+            resolverCombate(origen, destino, personajeAMover, personajeDefensor);
+        } else {
+            // Movimiento simple a casilla vacía
+
+            // Mover el personaje
+            destino.asignarPersonaje(personajeAMover);
+            origen.quitarPersonaje();
+        }
+    }
+
+    private void resolverCombate(casillas casillaAtacante, casillas casillaDefensor,
+            Fichas atacante, Fichas defensor) {
+        int rangoAtacante = atacante.getRango();
+        int rangoDefensor = defensor.getRango();
+
+        // Casos especiales
+        if (rangoAtacante == 0 && rangoDefensor == 10) {
+            // Nova Blast/Pumpkin Bomb puede derrotar al rango 10
+            casillaDefensor.asignarPersonaje(atacante);
+            casillaAtacante.quitarPersonaje();
+        } else if (rangoAtacante == 10 && rangoDefensor == 0) {
+            // Rango 10 es derrotado por Nova Blast/Pumpkin Bomb
+            // El defensor se queda en su lugar, el atacante es eliminado
+            casillaAtacante.quitarPersonaje();
+        } else if (rangoAtacante > rangoDefensor) {
+            // Atacante gana (rango mayor)
+            casillaDefensor.asignarPersonaje(atacante);
+            casillaAtacante.quitarPersonaje();
+        } else if (rangoAtacante < rangoDefensor) {
+            // Defensor gana (rango mayor)
+            // El defensor se queda en su lugar, el atacante es eliminado
+            casillaAtacante.quitarPersonaje();
+        } else {
+            // Empate - ambos son eliminados
+            casillaAtacante.quitarPersonaje();
+            casillaDefensor.quitarPersonaje();
+        }
+    }
+
+    // Método para cancelar selección actual
+    public void cancelarSeleccion() {
+        primeraCasillaSelecc = null;
+        esperarSegundoClic = false;
+    }
+
+    // Método para obtener información del estado actual
+    public void mostrarEstadoSeleccion() {
+        if (esperarSegundoClic && primeraCasillaSelecc != null) {
+            System.out.println("Personaje seleccionado: "
+                    + primeraCasillaSelecc.getPersonaje().getNombrePersonaje()
+                    + " en [" + primeraCasillaSelecc.getFila() + "][" + primeraCasillaSelecc.getColumna() + "]");
+        } else {
+            System.out.println("Ningún personaje seleccionado");
+        }
     }
 
 }
